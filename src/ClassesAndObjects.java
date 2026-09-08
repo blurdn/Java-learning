38799469b99dbbe99d5f1418f89fe7a6f400a75e
blury <rubakruta@gmail.com>
2026-09-08 23:52:00 +0500

    Now with encapsulation!

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -2,31 +2,57 @@ public class ClassesAndObjects {
     public static void main(String[] args) {
         Person me = new Person();
         me.setName("Dmitrii");
-        me.age = 19; // bad encapsulation or smth
-        me.introLine = "uhhh idk";
+        me.setAge(19);
+        me.setIntroLine("uhhh idk");
 
         me.introduction();
 
         Person trickyTony = new Person();
-        trickyTony.name = "Toby \"Don't call me Radiation\" Radiation Fox";
-        trickyTony.age = 34;
-        trickyTony.introLine = "am so tricky";
+        trickyTony.setName("Toby \"Don't call me Radiation\" Radiation Fox");
+        trickyTony.setAge(34);
+        trickyTony.setIntroLine("am so tricky");
 
         trickyTony.introduction();
 
         int allAgesDoubledProduct = me.ageButDoubled() * trickyTony.ageButDoubled();
         System.out.println("big numba: " + allAgesDoubledProduct);
         System.out.println("potentially bigger numba: " + me.ageButCustomMultiplier(42424242));
+        System.out.println("also I need to get something using get method but don't really have a creative reason to do so here it is: " + me.getName());
     }
 }
 
 class Person {
-    String name;
-    int age;
-    String introLine;
+    private String name;
+    private int age;
+    private String introLine;
 
-    void setName(String username) {
-        name = username;
+    public void setName(String username) {
+        if (!username.isEmpty()) {
+            name = username;
+        }
+        else {
+            System.out.println("can't set an empty name why would you do that");
+        }
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setIntroLine(String introLine) {
+        this.introLine = introLine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getIntroLine() {
+        return introLine;
     }
 
     int ageButDoubled() {
