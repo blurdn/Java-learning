2fc9896f0aa8e92eda749b0817c4c16afbf1809e
blury <rubakruta@gmail.com>
2026-09-13 00:12:22 +0500

    string formatting

--- src/StringsLesson.java
+++ src/StringsLesson.java
@@ -6,5 +6,12 @@ public class StringsLesson {
                 .append(" Welcome");
 
         System.out.println(sb);
+
+        System.out.printf("some formatted text %s\n", "HERE");
+        System.out.printf("and digits!! %s %d\n", "here:", 80085);
+        System.out.printf("also we got a looooot of space for digits (it's not used tho): %15d. nice\n", 42);
+        System.out.printf("and the other way around! %-15d. nice\n", 42);
+        System.out.printf("and we can round numbers: %.2f!!\n", 800.847023425);
+        System.out.println("r/unexpectedFactorial");
     }
 }
