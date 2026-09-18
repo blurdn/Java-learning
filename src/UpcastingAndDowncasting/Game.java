dfd74059fd63093af8ab166f342059c189340a70
blury <rubakruta@gmail.com>
2026-09-18 16:20:33 +0500

    Upcasting and Downcasting

---
+++ src/UpcastingAndDowncasting/Game.java
@@ -0,0 +1,7 @@
+package UpcastingAndDowncasting;
+
+public class Game {
+    public void lose() {
+        System.out.println("You lost The Game!\n");
+    }
+}
---
+++ src/UpcastingAndDowncasting/Test.java
@@ -0,0 +1,21 @@
+package UpcastingAndDowncasting;
+
+public class Test {
+    public static void main(String[] args) {
+        // Upcasting
+        Game game = new RPG();
+
+        RPG rpg = new RPG();
+        Game game1 = rpg;
+
+        // Downcasting
+        RPG rpg1 = (RPG) game;
+        rpg1.levelUp();
+
+        // Downcasting can cause errors!
+        Game genericGame = new Game();
+        RPG genericRPG = (RPG) genericGame;
+        genericRPG.levelUp(); // Exception! cuz conversion doesn't actually change object types, it only changes its link
+    }
+
+}
